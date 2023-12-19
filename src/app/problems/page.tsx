4d7ac94b3004f9f89ba1b5d2c3788d571468d2a1
Lootmann <chore.machines.10260930@gmail.com>
2023-12-20 06:46:20 +0900

    problems components

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -8,7 +8,8 @@ import { ProblemType } from "../types/Problem";
 import Link from "next/link";
 
 const table_row = `px-2 py-1 border text-center`;
-const hover_row = `hover:bg-lime-900`;
+const hover_row = `hover:bg-sky-800`;
+const link = `px-1 bg-red-400 hover:bg-red-600 text-slate-900 rounded-md`;
 
 export default function Page() {
   const [problems, setProblms] = React.useState<ProblemType[]>([]);
@@ -43,9 +44,7 @@ export default function Page() {
             <th className={table_row}>id</th>
             <th className={table_row}>Front</th>
             <th className={table_row}>Back</th>
-            <th className={table_row}>Deck Id</th>
-            <th className={table_row}>Deck Title</th>
-            <th className={table_row}>Edit</th>
+            <th className={table_row}>Deck</th>
           </tr>
         </thead>
 
@@ -55,10 +54,10 @@ export default function Page() {
               <td className={table_row}>{prob.id}</td>
               <td className={table_row}>{prob.front}</td>
               <td className={table_row}>{prob.back}</td>
-              <td className={table_row}>{prob.deck.id}</td>
-              <td className={table_row}>{prob.deck.title}</td>
               <td className={table_row}>
-                <Link href="/">ed</Link>
+                <Link href="/" className={`${link}`}>
+                  edit
+                </Link>
               </td>
             </tr>
           ))}
