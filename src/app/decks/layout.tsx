611f81526036206a08fd6eade33525e46ed54feb
Lootmann <chore.machines.10260930@gmail.com>
2023-12-20 05:04:38 +0900

    decks/: css modify

--- src/app/decks/layout.tsx
+++ src/app/decks/layout.tsx
@@ -1,13 +1,7 @@
-"use client";
-
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-full w-full flex flex-col gap-4 text-xl">
-      <div className="p-2 border rounded-md flex flex-col gap-4">
-        {children}
-      </div>
-    </div>
+    <div className="h-full w-full flex flex-col gap-4 text-xl">{children}</div>
   );
 }
--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -1,31 +1,30 @@
+/**
+ * decks/
+ */
 "use client";
 
 import React from "react";
-import { DeckType } from "../types/Deck";
-import { SubmitHandler, useForm } from "react-hook-form";
 import Link from "next/link";
+import { DeckType } from "../types/Deck";
 
-const table_row = `border px-2 py-1`;
+const inner = `py-4 px-8
+  h-full w-full flex flex-col flex-1 grow items-stretch
+  border border-neutral-200 rounded-md`;
 
-export default function Page({ children }: { children: React.ReactDOM }) {
+const deck_row = `text-xl px-2 py-1
+  flex gap-4
+  hover:bg-stone-800 rounded-md`;
+
+const footer = `py-4 px-8
+  w-full flex gap-4
+  border border-neutral-200 rounded-md`;
+
+const button = `p-2 text-xl text-bold
+  border rounded-md
+  hover:bg-neutral-200 hover:text-neutral-900 duration-100`;
+
+export default function Home() {
   const [decks, setDecks] = React.useState<DeckType[]>([]);
-  const [refresh, setRefresh] = React.useState<boolean>(false);
-  const { register, handleSubmit, reset } = useForm<DeckType>();
-
-  const onSubmit: SubmitHandler<DeckType> = async (data) => {
-    const options = {
-      method: "POST",
-      "Content-Type": "application/json",
-      body: JSON.stringify({ title: data.title }),
-    };
-
-    const resp = await fetch("/api/decks", options);
-    // const ret = await resp.json();
-    console.log(resp);
-
-    setRefresh(!refresh);
-    reset({ title: "" });
-  };
 
   React.useEffect(() => {
     const fetchDecks = async () => {
@@ -37,62 +36,34 @@ export default function Page({ children }: { children: React.ReactDOM }) {
       const resp = await fetch("/api/decks", options);
       const data = await resp.json();
 
+      if (!resp.ok) {
+        console.log(data);
+      }
+
       setDecks(data);
     };
 
     fetchDecks();
-  }, [refresh, reset]);
+  }, []);
 
   return (
-    <div className="h-full w-full flex flex-col gap-4 text-xl">
-      <div className="p-2 border rounded-md flex flex-col gap-4">
-        <h2 className="text-xl">Create Decks</h2>
-
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col gap-4 text-slate-100"
-        >
-          <input
-            type="text"
-            {...register("title")}
-            placeholder="Statement"
-            autoComplete="off"
-            className="pl-2 bg-slate-900 rounded-sm"
-          />
-
-          <input
-            type="submit"
-            value="Create"
-            className="bg-slate-700 rounded-sm"
-          />
-        </form>
+    <div className="h-full w-full flex flex-col gap-4 items-center">
+      <div className={`${inner}`}>
+        {decks.map((deck) => (
+          <div key={deck.id} className={`${deck_row}`}>
+            <p className="grow">{deck.title}</p>
+            <p>problems</p>
+            <p>edit</p>
+            <p>delete</p>
+          </div>
+        ))}
       </div>
 
-      <div className="border p-2 rounded-md">
-        <h2 className="text-xl mb-2">All Decks</h2>
-
-        <table className="table-auto">
-          <thead>
-            <tr className={table_row}>
-              <th className={table_row}>id</th>
-              <th className={table_row}>title</th>
-              <th className={table_row}>Edit</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {decks.map((deck) => (
-              <tr key={deck.id} className={table_row}>
-                <td className={table_row}>{deck.id}</td>
-                <td className={table_row}>{deck.title}</td>
-                <td className={table_row}>
-                  <Link href={`/decks/${deck.id}`}>edit</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <footer className={`${footer}`}>
+        <Link href={`/decks/create/`} className={`${button}`}>
+          Create Deck
+        </Link>
+      </footer>
     </div>
   );
 }
