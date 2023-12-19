d1ddfbe0152202e5c326fdacdf3f149286550261
Lootmann <chore.machines.10260930@gmail.com>
2023-12-19 19:30:13 +0900

    deck/deck_id component

--- src/app/decks/[id]/layout.tsx
+++ src/app/decks/[id]/layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="h-full w-full flex flex-col gap-4 text-xl">{children}</div>
-  );
+  return <>{children}</>;
 }
--- src/app/decks/[id]/page.tsx
+++ src/app/decks/[id]/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+"use client";
+
+import { DeckType } from "@/app/types/Deck";
+import React from "react";
+
+type Props = {
+  params: {
+    deckId: string;
+  };
+};
+
+export default function Page({ params }: Props) {
+  const [deck, setDeck] = React.useState<DeckType>();
+
+  React.useEffect(() => {
+    const fetchDecks = async () => {
+      const options = {
+        method: "GET",
+        "Content-Type": "application/json",
+      };
+
+      const resp = await fetch("/api/decks/1", options);
+      const data = await resp.json();
+      setDeck(data);
+    };
+
+    fetchDecks();
+  }, []);
+
+  return (
+    <div>
+      <h2>{params.deckId}</h2>
+      <p>{deck?.statement}</p>
+      <p>{deck?.answer}</p>
+    </div>
+  );
 }
