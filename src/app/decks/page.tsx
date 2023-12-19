0b8fcd787cc74d83c3ce4d972c562c009e297016
Lootmann <chore.machines.10260930@gmail.com>
2023-12-19 19:44:34 +0900

    decks main

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -3,6 +3,9 @@
 import React from "react";
 import { DeckType } from "../types/Deck";
 import { SubmitHandler, useForm } from "react-hook-form";
+import Link from "next/link";
+
+const table_row = `border px-2 py-1`;
 
 export default function Page({ children }: { children: React.ReactDOM }) {
   const [decks, setDecks] = React.useState<DeckType[]>([]);
@@ -17,10 +20,9 @@ export default function Page({ children }: { children: React.ReactDOM }) {
     };
 
     const resp = await fetch("/api/decks", options);
+    // const ret = await resp.json();
     console.log(resp);
 
-    const ret = await resp.json();
-    console.log(ret);
     setRefresh(!refresh);
   };
 
@@ -30,10 +32,10 @@ export default function Page({ children }: { children: React.ReactDOM }) {
         method: "GET",
         "Content-Type": "application/json",
       };
+
       const resp = await fetch("/api/decks", options);
       const data = await resp.json();
-      console.log(resp);
-      console.log(data);
+
       setDecks(data);
     };
 
@@ -76,12 +78,31 @@ export default function Page({ children }: { children: React.ReactDOM }) {
       <div className="border p-2 rounded-md">
         <h2 className="text-xl mb-2">All Decks</h2>
 
+        <table className="table-auto">
+          <thead>
+            <tr className={table_row}>
+              <th className={table_row}>id</th>
+              <th className={table_row}>statement</th>
+              <th className={table_row}>answer</th>
+              <th className={table_row}>Edit</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {decks.map((deck) => (
+              <tr key={deck.id} className={table_row}>
+                <td className={table_row}>{deck.id}</td>
+                <td className={table_row}>{deck.statement}</td>
+                <td className={table_row}>{deck.answer}</td>
+                <td className={table_row}>
+                  <Link href={`/decks/${deck.id}`}>edit</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
         <ul>
-          {decks.map((deck) => (
-            <li key={deck.id} className="underline underline-offset-2">
-              {deck.statement}, {deck.answer}
-            </li>
-          ))}
           <li></li>
         </ul>
       </div>
