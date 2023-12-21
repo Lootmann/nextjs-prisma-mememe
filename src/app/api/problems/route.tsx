73d3ad3fe8583cb1d97d79486b95bba631a130d9
Lootmann <chore.machines.10260930@gmail.com>
2023-12-22 07:26:24 +0900

    move crud function to /app/crud/

--- src/app/api/problems/route.tsx
+++ src/app/api/problems/route.tsx
@@ -1,31 +1,17 @@
+import { createProblem, getProblems } from "@/app/crud/problem";
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const decks = await getAllProblems();
+export async function GET(req: NextRequest) {
+  const decks = await getProblems();
   return NextResponse.json(decks);
 }
 
-export async function POST(request: NextResponse) {
-  const { front, back, deckId } = await request.json();
-
-  await prisma.problem.create({
-    data: {
-      front: front,
-      back: back,
-      deckId: Number(deckId),
-    },
-  });
-
+export async function POST(req: NextRequest) {
+  // get params from POST body
+  const { front, back, deckId } = await req.json();
+  await createProblem(front, back, deckId);
   return NextResponse.json({ msg: "success" }, { status: 201 });
 }
-
-const getAllProblems = async () => {
-  return await prisma.problem.findMany({
-    include: {
-      deck: true,
-    },
-  });
-};
