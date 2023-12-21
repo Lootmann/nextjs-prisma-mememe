f2a082e2174d15b3c266758040e5d142fab26ce2
Lootmann <chore.machines.10260930@gmail.com>
2023-12-22 07:20:11 +0900

    add get random problem function to crud

--- src/app/crud/problem.tsx
+++ src/app/crud/problem.tsx
@@ -10,6 +10,24 @@ export const getProblemById = async (problemId: string) => {
   return await prisma.problem.findFirst({ where: { id: Number(problemId) } });
 };
 
+const randomIdx = (length: number) => {
+  return Math.floor(Math.random() * length);
+};
+
+export const getRandomProblemByDeck = async (deckId: string) => {
+  const deck = await prisma.deck.findFirst({
+    where: { id: Number(deckId) },
+    select: { problems: true },
+  });
+
+  if (deck) {
+    const idx = randomIdx(deck.problems.length);
+    return deck.problems[idx];
+  } else {
+    return {};
+  }
+};
+
 export const putProblem = async (id: string, front: string, back: string) => {
   return await prisma.problem.update({
     where: { id: Number(id) },
