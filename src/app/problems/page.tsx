f09363916516fb782ce53f46a760a6792c52c1e6
Lootmann <chore.machines.10260930@gmail.com>
2023-12-22 21:05:19 +0900

    show problem detail on left side, on the other side, right has all problems list

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -7,12 +7,29 @@ import React from "react";
 import { ProblemType } from "../types/Problem";
 import Link from "next/link";
 
-const table_row = `px-2 py-1 border text-center`;
+const left = `
+  h-full w-full flex flex-col gap-4`;
+const right = `
+  text-base
+  h-full w-full flex flex-col`;
+
+const table_row = `p-1 border text-center`;
 const hover_row = `hover:bg-sky-800`;
 const link = `px-1 bg-red-400 hover:bg-red-600 text-slate-900 rounded-md`;
 
+type PType = {
+  id: number;
+  front: string;
+  back: string;
+};
+
 export default function Page() {
-  const [problems, setProblms] = React.useState<ProblemType[]>([]);
+  const [problems, setProblems] = React.useState<ProblemType[]>([]);
+  const [problem, setProblem] = React.useState<PType>({
+    id: 0,
+    front: ":^)",
+    back: ":^)",
+  });
 
   React.useEffect(() => {
     const fetchProblems = async () => {
@@ -28,41 +45,69 @@ export default function Page() {
         console.log(data);
       }
 
-      setProblms(data);
+      setProblems(data);
     };
 
     fetchProblems();
   }, []);
 
+  const handleClick = (problemId: number) => {
+    const fetchProblem = async () => {
+      const options = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      };
+      const resp = await fetch(`/api/problems/${problemId}`, options);
+      const data = await resp.json();
+
+      if (!resp.ok) {
+        setProblem({ id: 0, front: ":^)", back: ":^)" });
+        return;
+      }
+      setProblem(data);
+    };
+
+    fetchProblem();
+  };
+
   return (
-    <div className="h-full w-full flex flex-col gap-4 items-center">
-      <h1 className="mb-4">Problems</h1>
+    <div className="h-full w-full flex  gap-4 items-center">
+      <div className={`${left}`}>
+        <p className="border p-2 grow">{problem.front}</p>
+        <p className="border p-2 grow">{problem.back}</p>
+      </div>
 
-      <table className="table-auto">
-        <thead className={table_row}>
-          <tr>
-            <th className={table_row}>id</th>
-            <th className={table_row}>Front</th>
-            <th className={table_row}>Back</th>
-            <th className={table_row}>Deck</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {problems.map((prob) => (
-            <tr key={prob.id} className={hover_row}>
-              <td className={table_row}>{prob.id}</td>
-              <td className={table_row}>{prob.front}</td>
-              <td className={table_row}>{prob.back}</td>
-              <td className={table_row}>
-                <Link href={`problems/${prob.id}`} className={`${link}`}>
-                  edit
-                </Link>
-              </td>
+      <div className={`${right}`}>
+        <table className="table-auto">
+          <thead className={table_row}>
+            <tr>
+              <th className={table_row}>id</th>
+              <th className={table_row}>Front</th>
+              <th className={table_row}>Back</th>
+              <th className={table_row}>Deck</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {problems.map((prob) => (
+              <tr
+                key={prob.id}
+                className={hover_row}
+                onClick={() => handleClick(prob.id)}
+              >
+                <td className={table_row}>{prob.id}</td>
+                <td className={table_row}>{prob.front}</td>
+                <td className={table_row}>{prob.back}</td>
+                <td className={table_row}>
+                  <Link href={`problems/${prob.id}`} className={`${link}`}>
+                    edit
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
