bbfafd7e1be0365a746d2720f19e2b830fb1770f
Lootmann <chore.machines.10260930@gmail.com>
2023-12-23 12:27:39 +0900

    update deck: add component and API

--- src/app/crud/deck.tsx
+++ src/app/crud/deck.tsx
@@ -7,11 +7,25 @@ export const getDecks = async () => {
 };
 
 export const getDeckById = async (deckId: number) => {
-  return await prisma.deck.findFirst({ where: { id: deckId } });
+  return await prisma.deck.findFirst({
+    where: { id: deckId },
+    include: { problems: true },
+  });
 };
 
 export const createDeck = async (title: string) => {
   return await prisma.deck.create({
+    data: {
+      title: title,
+    },
+  });
+};
+
+export const updateDeck = async (deckId: string, title: string) => {
+  return await prisma.deck.update({
+    where: {
+      id: Number(deckId),
+    },
     data: {
       title: title,
     },
