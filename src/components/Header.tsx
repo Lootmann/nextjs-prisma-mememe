556bc05c5b3000f0d7a30be185e37d28112c5cf1
Lootmann <chore.machines.10260930@gmail.com>
2023-12-23 17:59:05 +0900

    add new Icon to header, but it sucks.
    I create this, that's why.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="p-2 flex bg-neutral-700 justify-center">
-      <nav className="flex gap-4">
-        <Link href={"/"}>Top</Link>
+    <header className="py-1 flex bg-neutral-700">
+      <nav className="flex gap-4 mx-auto text-xl items-center">
+        {/* what the hell */}
+        <img
+          src="/images/header_icon.png"
+          className="h-9 px-1 py-1 mr-6 bg-white"
+        />
         <Link href={"/add/"}>Add</Link>
         <Link href={"/decks/"}>Decks</Link>
         <Link href={"/problems/"}>Problems</Link>
