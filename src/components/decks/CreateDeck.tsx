89ebd11b7d9b90c7be1d60b1f5e40278ed92240f
Lootmann <chore.machines.10260930@gmail.com>
2023-12-23 17:14:27 +0900

    Add slide-in animation to create deck form
    and show error messge from validating react-hook-form

--- src/components/decks/CreateDeck.tsx
+++ src/components/decks/CreateDeck.tsx
@@ -12,17 +12,18 @@ type Props = {
 };
 
 export function CreateDeck(props: Props) {
-  // fixme: add animation
   const wrapper = `
-    h-full w-full absolute top-0 left-0 z-10
+    h-full w-full absolute top-0 z-10
     flex justify-center
     bg-zinc-800 bg-opacity-60`;
 
   const form = `
+    relative
     h-44 px-2 mx-auto mt-10
     flex flex-col gap-4 justify-center items-center
     bg-slate-700
-    border border-slate-400 rounded-md`;
+    border border-slate-400 rounded-md
+    animate-slideInDown`;
 
   const input = `
     px-2
@@ -30,9 +31,12 @@ export function CreateDeck(props: Props) {
     text-neutral-900
     rounded-md`;
 
-  const { register, handleSubmit } = useForm<DeckCreateType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DeckCreateType>();
 
-  // fixme: create new Deck
   const onSubmit: SubmitHandler<DeckCreateType> = async (data) => {
     console.log(data);
     const options = {
@@ -42,6 +46,7 @@ export function CreateDeck(props: Props) {
     };
     const resp = await fetch("/api/decks", options);
 
+    // fixme: add validation
     if (!resp.ok) {
       console.log(resp);
     }
@@ -51,23 +56,26 @@ export function CreateDeck(props: Props) {
   };
 
   return (
-    // fixme: animation - showing top-bottom, disappearing bottom-top
-    <div className={wrapper} onClick={props.handleShowCreateDeck}>
+    <div className={`${wrapper}`} onClick={props.handleShowCreateDeck}>
       <form
         className={form}
         onSubmit={handleSubmit(onSubmit)}
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className="text-xl underline underline-offset-4">Create Deck</h2>
-        <p>
-          Input Title. Empty is NOT allowed.
-          <br />
-          Dup is allowed, but it will confused you :^)
-        </p>
 
-        {/* fixme: react-hook-form validation: required */}
+        {errors.title ? (
+          <p className="text-red-500">Empty is Not Allowed ヽ(｀Д´#)ﾉ</p>
+        ) : (
+          <p>
+            Input Deck title. Empty is not allowed.
+            <br />
+            Dup is allowed, but it will confuse you :^&#41;
+          </p>
+        )}
+
         <input
-          {...register("title")}
+          {...register("title", { required: true })}
           className={input}
           placeholder="input title"
         />
