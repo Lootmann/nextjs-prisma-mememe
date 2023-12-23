cd0ade771790e5125ab4598d817745e7f4097af3
Lootmann <chore.machines.10260930@gmail.com>
2023-12-23 17:05:11 +0900

    add slide-in-down animation for create deck

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        slideInDown: "slideIn 0.2s ease-in-out",
+      },
+      keyframes: {
+        slideIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-80px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
