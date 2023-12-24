0fb0129a31a455ac28485d85093dc1c7780635a3
Lootmann <chore.machines.10260930@gmail.com>
2023-12-25 00:16:04 +0900

    delete edit column from table

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -5,17 +5,13 @@
 
 import React from "react";
 import { ProblemType } from "../../types/Problem";
-import Link from "next/link";
 
-const left = `
-  h-full w-full flex flex-col gap-4`;
 const right = `
   text-base
   h-full w-full flex flex-col`;
 
 const table_row = `p-1 border text-center`;
 const hover_row = `hover:bg-sky-800`;
-const link = `px-1 bg-red-400 hover:bg-red-600 text-slate-900 rounded-md`;
 
 type PType = {
   id: number;
@@ -84,7 +80,6 @@ export default function Page() {
               <th className={table_row}>id</th>
               <th className={table_row}>Front</th>
               <th className={table_row}>Back</th>
-              <th className={table_row}>Deck</th>
             </tr>
           </thead>
 
@@ -98,11 +93,6 @@ export default function Page() {
                 <td className={table_row}>{prob.id}</td>
                 <td className={table_row}>{prob.front}</td>
                 <td className={table_row}>{prob.back}</td>
-                <td className={table_row}>
-                  <Link href={`problems/${prob.id}`} className={`${link}`}>
-                    edit
-                  </Link>
-                </td>
               </tr>
             ))}
           </tbody>
